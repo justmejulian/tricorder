ec6432ca94029dec06dce4cd31cec86286941c2e
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2024-10-01 14:44:47 +0200

    Initial Commit

---
+++ tricorder Watch App/tricorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  tricorderApp.swift
+//  tricorder Watch App
+//
+//  Created by Julian Visser on 01.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct tricorder_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

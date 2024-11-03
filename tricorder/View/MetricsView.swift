549c8af5942d90a4b9fdf33bd794be2464915625
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2024-11-03 12:22:11 +0100

    moved into own files

---
+++ tricorder/View/MetricsView.swift
@@ -0,0 +1,18 @@
+//
+//  MetricsView.swift
+//  tricorder
+//
+//  Created by Julian Visser on 03.11.2024.
+//
+
+import SwiftUI
+
+struct MetricsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MetricsView()
+}
---
+++ tricorder/View/StartStopRecordingButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  StartStopRecordingButtonView.swift
+//  tricorder
+//
+//  Created by Julian Visser on 03.11.2024.
+//
+
+import SwiftUI
+
+struct StartStopRecordingButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartStopRecordingButtonView()
+}

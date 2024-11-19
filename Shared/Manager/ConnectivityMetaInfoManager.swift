e2c1b280d57e9b15d32697f047cbd093207c1d50
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2024-11-19 16:34:09 +0100

    optimise disabling while wait for packets

--- Shared/Manager/ConnectivityMetaInfoManager.swift
+++ Shared/Manager/ConnectivityMetaInfoManager.swift
@@ -17,13 +17,12 @@ class ConnectivityMetaInfoManager: ObservableObject {
         openSendConnectionsCount > 0
     }
 
-    @Published
     var lastDidReceiveDataDate: Date?
 
     @Published
     var isLastDidReceiveDataDateTooRecent = false
 
-    private let debouncer = Debouncer(duration: .seconds(5))  // Wait for last packages
+    private let debouncer = Debouncer(duration: .seconds(10))  // Wait for last packages
 }
 
 extension ConnectivityMetaInfoManager {
@@ -44,24 +43,21 @@ extension ConnectivityMetaInfoManager {
         Logger.shared.debug("\(#function) called on Thread \(Thread.current)")
 
         lastDidReceiveDataDate = Date()
-        Task {
-            await updateIsLastDidReceiveDataDateTooRecent()
-        }
+        updateIsLastDidReceiveDataDateTooRecent()
     }
 
-    func updateIsLastDidReceiveDataDateTooRecent() async {
+    func updateIsLastDidReceiveDataDateTooRecent() {
         Logger.shared.debug("\(#function) called on Thread \(Thread.current)")
 
-        Logger.shared.debug("Setting lastDidReceiveDataDateTooRecent to true")
-        isLastDidReceiveDataDateTooRecent = true
-        guard await debouncer.sleep() else { return }
-        Logger.shared.debug("Setting lastDidReceiveDataDateTooRecent to false")
-        guard let lastDidReceiveDataDate else {
+        Task {
+            Logger.shared.debug("Setting lastDidReceiveDataDateTooRecent to true")
             isLastDidReceiveDataDateTooRecent = true
-            return
+            
+            // Only continue if no more updates
+            guard await debouncer.sleep() else { return }
+
+            Logger.shared.debug("Setting lastDidReceiveDataDateTooRecent to false")
+            isLastDidReceiveDataDateTooRecent = false
         }
-        let timeInterval = lastDidReceiveDataDate.timeIntervalSinceNow
-        // More that 5s ago
-        isLastDidReceiveDataDateTooRecent = timeInterval.isLess(than: -5)
     }
 }

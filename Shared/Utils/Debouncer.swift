dc610d01a2bbac74d1893814353671f5fe3207fe
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2024-12-07 14:46:45 +0100

    improve debounce

--- Shared/Utils/Debouncer.swift
+++ Shared/Utils/Debouncer.swift
@@ -1,25 +1,37 @@
 //
 //  Debouncer.swift
-//  tricorder
-//
-//  code from: https://stackoverflow.com/questions/76887491/swift-observation-framework-and-debounce
 //
 //  Created by Julian Visser on 15.11.2024.
 //
 
+import OSLog
+
 actor Debouncer {
     private let duration: Duration
-    private var isPending = false
+    private var currentTask: Task<(), any Error>?
 
     public init(duration: Duration) {
         self.duration = duration
     }
 
     public func sleep() async -> Bool {
-        if isPending { return false }
-        isPending = true
-        try? await Task.sleep(for: duration)
-        isPending = false
-        return true
+        // Cancel the currently pending task, if any
+        currentTask?.cancel()
+
+        // Create a new task for this debounce cycle
+        let task = Task {
+            try await Task.sleep(for: duration)
+        }
+        
+        currentTask = task
+
+        // Await the task completion or cancellation
+        do {
+            try await task.value
+            currentTask = nil
+            return true  // Completed successfully
+        } catch {
+            return false
+        }
     }
 }
